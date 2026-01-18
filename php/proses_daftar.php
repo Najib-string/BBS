0e964bb15219f6eebea7257c3707517bf69133a5
ekky10223039-rgb <ekky10223039@student.sttcipasung.ac.id>
2026-01-18 21:06:19 +0700

    data ortu dan pendidikan

--- php/proses_daftar.php
+++ php/proses_daftar.php
@@ -17,3 +17,19 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $alamat        = mysqli_real_escape_string($koneksi, $_POST['alamat']);
     $no_hp         = mysqli_real_escape_string($koneksi, $_POST['no_hp']);
     $email         = mysqli_real_escape_string($koneksi, $_POST['email']);
+
+    // =====================
+    // DATA ORANG TUA
+    // =====================
+    $nama_ayah      = mysqli_real_escape_string($koneksi, $_POST['nama_ayah']);
+    $pekerjaan_ayah = mysqli_real_escape_string($koneksi, $_POST['pekerjaan_ayah']);
+    $nama_ibu       = mysqli_real_escape_string($koneksi, $_POST['nama_ibu']);
+    $pekerjaan_ibu  = mysqli_real_escape_string($koneksi, $_POST['pekerjaan_ibu']);
+    $no_hp_ortu     = mysqli_real_escape_string($koneksi, $_POST['no_hp_ortu']);
+
+    // =====================
+    // DATA PENDIDIKAN
+    // =====================
+    $asal_sekolah = mysqli_real_escape_string($koneksi, $_POST['asal_sekolah']);
+    $tahun_lulus  = mysqli_real_escape_string($koneksi, $_POST['tahun_lulus']);
+    $alasan       = mysqli_real_escape_string($koneksi, $_POST['alasan_daftar']);
